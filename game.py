b3243a36a260f07d449d231b8504b1e819cf87a7
joeyissa <joeyissa59@gmail.com>
2023-08-28 13:47:45 -0400

    story scenes are beginning

--- game.py
+++ game.py
@@ -84,6 +84,8 @@ class Game:
         # screen shake
         self.screenshake = 0
 
+        self.story_timer = 500
+
 
     def load_level(self, map_id):
         self.tilemap.load('data/maps/' + str(map_id) + '.json')
@@ -130,146 +132,157 @@ class Game:
 
         # creating an infinite game loop
         while True:
-            self.display.fill((0, 0, 0, 0))    # outlines
-            # clear the screen for new image generation in loop
-            self.display_2.blit(self.assets['background'], (0,0)) # no outline
+            if self.story_timer > 500 and self.story_timer < 300:
+                self.display.fill((0, 0, 0, 0))    # outlines
+                # clear the screen for new image generation in loop
+                self.display_2.blit(self.assets['Story1'], (0,0)) # no outline
+            elif self.story_timer > 300:
+                self.display.fill((0, 0, 0, 0))    # outlines
+                # clear the screen for new image generation in loop
+                self.display_2.blit(self.assets['Story2'], (0,0)) # no outline
+            else:
+                self.display.fill((0, 0, 0, 0))    # outlines
+                # clear the screen for new image generation in loop
+                self.display_2.blit(self.assets['background'], (0,0)) # no outline
 
-            self.screenshake = max(0, self.screenshake-1) # resets screenshake value
+                self.screenshake = max(0, self.screenshake-1) # resets screenshake value
 
-            # level transiition
-            if not len(self.enemies): # no enemies
-                self.transition += 1 # start timer, increasing value past 0
-                if self.transition > 30: 
-                    self.level = min(self.level + 1, self.max_level) # increase level
-                    self.load_level(self.level) # -1 since we start at level 0
-            if self.transition < 0:
-                self.transition += 1 # goes up automatically until 0
 
-            if self.dead: # get hit once
-                self.dead += 1
-                if self.dead >= 10: # to make the level transitions smoother
-                    self.transition = min(self.transition + 1, 30) # go as high as it can without changing level
-                if self.dead > 40: # timer that starts when you die
-                    self.load_level(self.level)
 
-            # move 'camera' to focus on player, make him the center of the screen
-            # scroll = current scroll + (where we want the camera to be - what we have/can see currently) 
-            self.scroll[0] += (self.player.rect().centerx - self.display.get_width()/2 - self.scroll[0])  / 30  # x axis
-            self.scroll[1] += (self.player.rect().centery - self.display.get_height()/2 - self.scroll[1]) / 30
+                # level transiition
+                if not len(self.enemies): # no enemies
+                    self.transition += 1 # start timer, increasing value past 0
+                    if self.transition > 30: 
+                        self.level = min(self.level + 1, self.max_level) # increase level
+                        self.load_level(self.level) # -1 since we start at level 0
+                if self.transition < 0:
+                    self.transition += 1 # goes up automatically until 0
 
-            # fix the jitter
-            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+                if self.dead: # get hit once
+                    self.dead += 1
+                    if self.dead >= 10: # to make the level transitions smoother
+                        self.transition = min(self.transition + 1, 30) # go as high as it can without changing level
+                    if self.dead > 40: # timer that starts when you die
+                        self.load_level(self.level)
 
-            # spawn particles
-            for rect in self.leaf_spawners:
-                if random.random() * 49999 < rect.width * rect.height:
-                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
-                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+                # move 'camera' to focus on player, make him the center of the screen
+                # scroll = current scroll + (where we want the camera to be - what we have/can see currently) 
+                self.scroll[0] += (self.player.rect().centerx - self.display.get_width()/2 - self.scroll[0])  / 30  # x axis
+                self.scroll[1] += (self.player.rect().centery - self.display.get_height()/2 - self.scroll[1]) / 30
 
-            self.clouds.update() # updates clouds before the rest of the tiles
-            self.clouds.render(self.display_2, offset=render_scroll)
+                # fix the jitter
+                render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-            self.tilemap.render(self.display, offset=render_scroll)
+                # spawn particles
+                for rect in self.leaf_spawners:
+                    if random.random() * 49999 < rect.width * rect.height:
+                        pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                        self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
 
-            # render the enemies
-            for enemy in self.enemies.copy():
-                kill =  enemy.update(self.tilemap, (0,0))
-                enemy.render(self.display, offset=render_scroll)
-                if kill: # if enemies update fn returns true [**]
-                    self.enemies.remove(enemy) 
+                self.clouds.update() # updates clouds before the rest of the tiles
+                self.clouds.render(self.display_2, offset=render_scroll)
 
-            if not self.dead:
-                # update player movement
-                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-                self.player.render(self.display, offset=render_scroll)
+                self.tilemap.render(self.display, offset=render_scroll)
 
-            # render/spawn bullet projectiles
-            # [[x, y], direction, timer]
-            for projectile in self.projectiles.copy():
-                projectile[0][0] += projectile[1]
-                projectile[2] += 1
-                img = self.assets['projectile']
-                self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1])) # spawns it the center of the projectile
-                
-                if self.tilemap.solid_check(projectile[0]): # if location is a solid tile
-                    self.projectiles.remove(projectile)
-                    for i in range(4):
-                        self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random())) # (math.pi if projectile[1] > 0 else 0), sparks bounce in oppositie direction if hit wall which depends on projectile direction
-                elif projectile[2] > 360: #if timer > 6 seconds
-                    self.projectiles.remove(projectile)
-                elif abs(self.player.dashing) < 50: # if not in dash
-                    if self.player.rect().collidepoint(projectile[0]):
+                # render the enemies
+                for enemy in self.enemies.copy():
+                    kill =  enemy.update(self.tilemap, (0,0))
+                    enemy.render(self.display, offset=render_scroll)
+                    if kill: # if enemies update fn returns true [**]
+                        self.enemies.remove(enemy) 
+
+                if not self.dead:
+                    # update player movement
+                    self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                    self.player.render(self.display, offset=render_scroll)
+
+                # render/spawn bullet projectiles
+                # [[x, y], direction, timer]
+                for projectile in self.projectiles.copy():
+                    projectile[0][0] += projectile[1]
+                    projectile[2] += 1
+                    img = self.assets['projectile']
+                    self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1])) # spawns it the center of the projectile
+                    
+                    if self.tilemap.solid_check(projectile[0]): # if location is a solid tile
                         self.projectiles.remove(projectile)
-                        self.dead += 1
-                        self.sfx['hit'].play()
-                        self.screenshake = max(16, self.screenshake)  # apply screenshake, larger wont be overrided by a smaller screenshake
-                        for i in range(30): # when projectile hits player
-                            # on death sparks
-                            angle = random.random() * math.pi * 2 # random angle in a circle
-                            speed = random.random() * 5
-                            self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random())) 
-                            # on death particles
-                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle +math.pi) * speed * 0.5, math.sin(angle * math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+                        for i in range(4):
+                            self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random())) # (math.pi if projectile[1] > 0 else 0), sparks bounce in oppositie direction if hit wall which depends on projectile direction
+                    elif projectile[2] > 360: #if timer > 6 seconds
+                        self.projectiles.remove(projectile)
+                    elif abs(self.player.dashing) < 50: # if not in dash
+                        if self.player.rect().collidepoint(projectile[0]):
+                            self.projectiles.remove(projectile)
+                            self.dead += 1
+                            self.sfx['hit'].play()
+                            self.screenshake = max(16, self.screenshake)  # apply screenshake, larger wont be overrided by a smaller screenshake
+                            for i in range(30): # when projectile hits player
+                                # on death sparks
+                                angle = random.random() * math.pi * 2 # random angle in a circle
+                                speed = random.random() * 5
+                                self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random())) 
+                                # on death particles
+                                self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle +math.pi) * speed * 0.5, math.sin(angle * math.pi) * speed * 0.5], frame=random.randint(0, 7)))
 
-            # spark affect
-            for spark in self.sparks.copy():
-                kill = spark.update()
-                spark.render(self.display, offset=render_scroll)
-                if kill:
-                    self.sparks.remove(spark)
+                # spark affect
+                for spark in self.sparks.copy():
+                    kill = spark.update()
+                    spark.render(self.display, offset=render_scroll)
+                    if kill:
+                        self.sparks.remove(spark)
 
-            # ouline based on display
-            display_mask = pygame.mask.from_surface(self.display)
-            display_sillhouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0)) # 180 opaque, 0 transparent
-            self.display_2.blit(display_sillhouette, (0, 0))
-            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                self.display_2.blit(display_sillhouette, offset) # putting what we drew onframe back into display
-            
+                # ouline based on display
+                display_mask = pygame.mask.from_surface(self.display)
+                display_sillhouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0)) # 180 opaque, 0 transparent
+                self.display_2.blit(display_sillhouette, (0, 0))
+                for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    self.display_2.blit(display_sillhouette, offset) # putting what we drew onframe back into display
+                
 
-            for particle in self.particles.copy():
-                kill = particle.update()
-                particle.render(self.display, offset=render_scroll)
-                if particle.type == 'leaf':
-                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3 # making the parlitcle move back and forth smooth'y
-                if kill:
-                    self.particles.remove(particle)
+                for particle in self.particles.copy():
+                    kill = particle.update()
+                    particle.render(self.display, offset=render_scroll)
+                    if particle.type == 'leaf':
+                        particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3 # making the parlitcle move back and forth smooth'y
+                    if kill:
+                        self.particles.remove(particle)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: # have to code the window closing
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_a: # referencing WASD
-                        self.movement[0] = True
-                    if event.key == pygame.K_d:
-                        self.movement[1] = True
-                    if event.key == pygame.K_w:
-                        if self.player.jump():  # velocity pointing upwards, gravity will pull player back down over time
-                            self.sfx['jump'].play()
-                    if event.key == pygame.K_e:
-                        self.player.dash()
-                if event.type == pygame.KEYUP: # when key is released
-                    if event.key == pygame.K_a: # referencing WASD
-                        self.movement[0] = False
-                    if event.key == pygame.K_d:
-                        self.movement[1] = False
-                    if event.key == pygame.K_w:
-                        self.movement[2] = False
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT: # have to code the window closing
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_a: # referencing WASD
+                            self.movement[0] = True
+                        if event.key == pygame.K_d:
+                            self.movement[1] = True
+                        if event.key == pygame.K_w:
+                            if self.player.jump():  # velocity pointing upwards, gravity will pull player back down over time
+                                self.sfx['jump'].play()
+                        if event.key == pygame.K_e:
+                            self.player.dash()
+                    if event.type == pygame.KEYUP: # when key is released
+                        if event.key == pygame.K_a: # referencing WASD
+                            self.movement[0] = False
+                        if event.key == pygame.K_d:
+                            self.movement[1] = False
+                        if event.key == pygame.K_w:
+                            self.movement[2] = False
 
 
-            # implementing transition
-            if self.transition:
-                transition_surf = pygame.Surface(self.display.get_size())
-                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8) # display center of screen, 30 is the timer we chose, 30 * 8 = 180
-                transition_surf.set_colorkey((255, 255, 255)) # making the circle transparent now
-                self.display_2.blit(transition_surf, (0, 0))
+                # implementing transition
+                if self.transition:
+                    transition_surf = pygame.Surface(self.display.get_size())
+                    pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8) # display center of screen, 30 is the timer we chose, 30 * 8 = 180
+                    transition_surf.set_colorkey((255, 255, 255)) # making the circle transparent now
+                    self.display_2.blit(transition_surf, (0, 0))
 
-            self.display_2.blit(self.display, (0, 0)) # cast display 2 on display
+                self.display_2.blit(self.display, (0, 0)) # cast display 2 on display
 
-            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
-            self.screen.blit(pygame.transform.scale(self.display_2, self.screen.get_size()), screenshake_offset) # render (now scaled) display image on big screen
-            pygame.display.update()
-            self.clock.tick(60) # run at 60 fps, like a sleep
+                screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
+                self.screen.blit(pygame.transform.scale(self.display_2, self.screen.get_size()), screenshake_offset) # render (now scaled) display image on big screen
+                pygame.display.update()
+                self.clock.tick(60) # run at 60 fps, like a sleep
 
 # returns the game then runs it
 Game().run()
