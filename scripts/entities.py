71c9caabc5bd24abc5036463258c610813e9f1d6
joeyissa <joeyissa59@gmail.com>
2023-08-30 17:17:24 -0400

    updated stun particles

--- scripts/entities.py
+++ scripts/entities.py
@@ -321,7 +321,7 @@ class Cat(PhysicsEntity):
                     angle = random.random() * math.pi * 2
                     speed = random.random() * 5
                     self.game.sparks.append(Spark(self.rect().center, angle, 2 + random.random()))
-                    self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+                    self.game.particles.append(Particle(self.game, 'particle_2', self.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
                 self.game.sparks.append(Spark(self.rect().center, 0, 5 + random.random()))
                 self.game.sparks.append(Spark(self.rect().center, math.pi, 5 + random.random()))
                 self.set_action('stun')
