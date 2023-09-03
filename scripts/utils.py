140d76b6250a20f430433b94ae26d2b7a8b33285
joeyissa <joeyissa59@gmail.com>
2023-09-03 15:33:17 -0400

    Finishing Touches, Submitted

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,7 +2,10 @@ import os
 
 import pygame
 
-BASE_IMG_PATH = 'data/images/'
+BASE_IMG_PATH = '/data/images/'
+
+
+# BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert_alpha()
